437be4b384569d5780841b366e145361511b3083
unknown <ccik@inbox.ru>
2013-04-21 21:02:50 +0400

    Added m_Bitmap field to class CtpDoc

--- tpDoc.h
+++ tpDoc.h
@@ -44,4 +44,7 @@ protected:
 	// Helper function that sets search content for a Search Handler
 	void SetSearchContent(const CString& value);
 #endif // SHARED_HANDLERS
+
+private:
+	CBitmap m_Bitmap;
 };
